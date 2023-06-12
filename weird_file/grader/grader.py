c72c03f775b7f86734057c465ea3bc66712d78bb
Will Snavely <will.snavely@protonmail.com>
2023-06-12 13:03:37 -0400

    Initial commmit

---
+++ weird_file/grader/grader.py
@@ -0,0 +1,6 @@
+def grade(arg, key):
+    key = key.upper()
+    if "THEINVISIBLEFENCEAROUNDTHEBLUEBERRYFARMWASAMYSTERIOUSBERRYCADE" in key:
+        return True, "Correct"
+    else:
+        return False, "Incorrect"
