df13460745f4ca6b121b2d51f6d3dc279e5bf06c
Ubuntu <ubuntu@ip-172-30-2-156.ec2.internal>
2023-06-22 02:32:38 +0000

    Simplify directory structure

--- weird_file/grader/grader.py
+++
@@ -1,6 +0,0 @@
-def grade(arg, key):
-    key = key.upper()
-    if "THEINVISIBLEFENCEAROUNDTHEBLUEBERRYFARMWASAMYSTERIOUSBERRYCADE" in key:
-        return True, "Correct"
-    else:
-        return False, "Incorrect"
